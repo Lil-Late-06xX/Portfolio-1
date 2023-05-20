928023fec1f6cfcc766d6174148f81ac7dfddd00
Houssam <111168411+Lil-Late-06xX@users.noreply.github.com>
2023-05-19 23:49:58 -0700

    Portfolio  update for use
    portfolio basic design i might need

---
+++ indexIO.js
@@ -0,0 +1,7 @@
+
+let toggle = ()=>{
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.phone-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
